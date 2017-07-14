bf102c5e40bebadc44fad1882c7ba248eed43fa8
Horia Coman <horia141@gmail.com>
2017-07-14 16:47:24 +0300

    Add new fields to the Creative model

--- StackExchange.Adzerk/Models/Creative.cs
+++ StackExchange.Adzerk/Models/Creative.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace StackExchange.Adzerk.Models
+﻿namespace StackExchange.Adzerk.Models
 {
     public class Creative
     {
         public long Id { get; set; }
         public long AdvertiserId { get; set; }
         public long AdTypeId { get; set; }
-
-        public string Body { get; set; }
-        public string Url { get; set; }
+        
         public string Title { get; set; }
+        public string ImageName { get; set; }
+        public string Url { get; set; }
+        public string Body { get; set; }
+        public string ScriptBody { get; set; }
+        public string ImageLink { get; set; }
         public string Alt { get; set; }
 
         public bool IsSync { get; set; }
         public bool IsDeleted { get; set; }
-        public bool IsActive { get; set; }
-
         public bool IsHTMLJS { get; set; }
-        public string ScriptBody { get; set; }
+        public bool IsActive { get; set; }
     }
 }
